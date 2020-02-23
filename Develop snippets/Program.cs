17302e4517b254430156b26a26f5c6571354d5f3
Kees Hiemstra <chi@xs4all.nl>
2020-02-23 09:58:09 +0100

    Develop snippets

---
+++ Develop snippets/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Develop_snippets
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("\nPress any key...");
+      Console.ReadKey();
+    }
+  }
+
+}
