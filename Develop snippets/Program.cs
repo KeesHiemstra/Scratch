9f73b44c37edcb88c742427fa8d1553b97ebb1e3
Kees Hiemstra <chi@xs4all.nl>
2020-02-24 12:18:54 +0100

    Experiment with try ... catch ... finally

--- Develop snippets/Program.cs
+++ Develop snippets/Program.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Develop_snippets
 {
@@ -12,8 +13,41 @@ namespace Develop_snippets
   {
     static void Main(string[] args)
     {
+      Console.WriteLine("Use return");
+      ExecuteTry(false);
+
+      Console.WriteLine("Use exit");
+      ExecuteTry(true);
+
       Console.Write("\nPress any key...");
       Console.ReadKey();
+    }
+
+    private static void ExecuteTry(bool UseExit)
+    {
+      try
+      {
+        int devide = 0;
+        Console.WriteLine(1 / devide);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show($"Exception {ex.Message}");
+        if (!UseExit)
+        {
+          return;
+        }
+        else
+        {
+          Environment.Exit(1);
+        }
+      }
+      finally
+      {
+        Console.WriteLine("Finally is executed");
+      }
+
+      Console.WriteLine("Never executed");
     }
   }
 
