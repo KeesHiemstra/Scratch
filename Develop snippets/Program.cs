37a6616e41b3e0ee9eb4a6bc47a6f11b87d891d6
Kees Hiemstra <chi@xs4all.nl>
2020-02-28 17:06:25 +0100

    Cleaned the experiment.

--- Develop snippets/Program.cs
+++ Develop snippets/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,42 +14,7 @@ namespace Develop_snippets
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Use return");
-      ExecuteTry(false);
-
-      Console.WriteLine("Use exit");
-      ExecuteTry(true);
-
       Console.Write("\nPress any key...");
       Console.ReadKey();
     }
-
-    private static void ExecuteTry(bool UseExit)
-    {
-      try
-      {
-        int devide = 0;
-        Console.WriteLine(1 / devide);
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show($"Exception {ex.Message}");
-        if (!UseExit)
-        {
-          return;
-        }
-        else
-        {
-          Environment.Exit(1);
-        }
-      }
-      finally
-      {
-        Console.WriteLine("Finally is executed");
-      }
-
-      Console.WriteLine("Never executed");
-    }
-  }
-
 }
