587c5e702905d3c7406218a97f0014c57a32663b
Kees Hiemstra <chi@xs4all.nl>
2020-07-19 14:25:47 +0200

    Cleanup

--- Develop snippets/Program.cs
+++ Develop snippets/Program.cs
@@ -15,34 +15,8 @@ namespace Develop_snippets
   {
     static void Main(string[] args)
     {
-      Test();
-
       Console.Write("\nPress any key...");
       Console.ReadKey();
-    }
-
-    private static void Test()
-    {
-      SystemEvents.PowerModeChanged += OnPowerChange;
-    }
-
-    private static void OnPowerChange(object sender, PowerModeChangedEventArgs e)
-    {
-      switch (e.Mode)
-      {
-        case PowerModes.Resume:
-          Console.WriteLine("Resume");
-          break;
-        case PowerModes.StatusChange:
-          Console.WriteLine("StatusChange");
-          break;
-        case PowerModes.Suspend:
-          Console.WriteLine("Suspend");
-          break;
-        default:
-          Console.WriteLine("Default");
-          break;
-      }
     }
   }
 }
