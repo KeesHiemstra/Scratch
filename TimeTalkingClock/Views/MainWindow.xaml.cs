6ffb558ea7f5b96ab9a98a76418da119c25a2a57
Kees Hiemstra <chi@xs4all.nl>
2020-08-13 07:51:34 +0200

    Developing TimeTalkingClock

---
+++ TimeTalkingClock/Views/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace TimeTalkingClock
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		//
+		// Implementation of INotifyPropertyChanged is not necessary to update the window
+		//
+
+		// "get" is necessary to update the window
+		public VisualTime Now { get; } = new VisualTime();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			// DataContext is necessary to update the window
+			DataContext = this;
+		}
+	}
+}
