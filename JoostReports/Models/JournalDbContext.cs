82b1b0c18cf6ec5512d7ec9371598d19e32ad7c8
Kees Hiemstra <chi@xs4all.nl>
2020-08-29 11:00:26 +0200

    Develop JournalReports

---
+++ JoostReports/Models/JournalDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace JoostReports.Models
+{
+	public class JournalDbContext : DbContext
+	{
+		public JournalDbContext(string dbConnection) : base(dbConnection) { }
+
+		public DbSet<Journal> Journals { get; set; }
+	}
+}
